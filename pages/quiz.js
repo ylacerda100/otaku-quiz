b34627ca33ca67e7510f1d08c09465a2e2f30619
Ygor Lacerda <ylacerda100@gmail.com>
2021-01-25 13:01:34 -0300

    Projeto quiz imersao alura nextjs primeiro comit

---
+++ pages/quiz.js
@@ -0,0 +1,72 @@
+import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+
+
+
+const Title = styled.h1`
+   font-size: 50px;
+   color: ${({ theme }) => theme.colors.primary};
+ `
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+
+`
+
+export const QuizContainer = styled.div`
+ width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
+
+
+
+export default function Quiz() {
+    return (
+  
+  
+        <BackgroundImage>
+          
+          <QuizContainer>
+            <Widget>
+  
+            <Widget.Header>
+              <h1>Otaku Test</h1>
+  
+            </Widget.Header>
+  
+              <Widget.Content>
+              <p>lorem ipsum dolor sit amet...</p>
+              </Widget.Content>
+            </Widget>
+             
+            <Widget>
+            <Widget.Header>
+              <h1>Otaku Test</h1>
+  
+            </Widget.Header>
+  
+              <Widget.Content>
+              <p>lorem ipsum dolor sit amet...</p>
+              </Widget.Content>
+            </Widget>
+            
+          </QuizContainer>
+          
+        </BackgroundImage>
+            
+            
+  
+        
+    );
+  }
+  
